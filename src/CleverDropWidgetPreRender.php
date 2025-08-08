7818c56d5faa6e45b6289e6620e7c0962dce6cb2
Jonathan Clancy <jclancy@clever-age.com>
2025-08-08 19:19:11 +0200

    Update theme for d10 compatibility

---
+++ src/CleverDropWidgetPreRender.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\cleverdrop;
+
+use Drupal\Core\Render\Element\RenderCallbackInterface;
+
+/**
+ * Provides a trusted callback
+ */
+class CleverDropWidgetPreRender implements RenderCallbackInterface {
+  public static function preRender($element) {
+    unset($element['#type']);
+    unset($element['#theme_wrappers']);
+    $element['#theme'] = 'widget_wrapper';
+
+    return $element;
+  }
+
+}
